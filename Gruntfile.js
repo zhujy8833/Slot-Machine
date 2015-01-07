a040ecace4773a853202b30d9295e16932940452
Jiangyue Zhu <zhujy8833@gmail.com>
2015-01-07 12:09:57 -0800

    new grunt build task

--- Gruntfile.js
+++ Gruntfile.js
@@ -8,11 +8,28 @@ module.exports = function(grunt) {
 		    }
 		  }
 		},
+        requirejs: {
+            compile : {
+                options : {
+                    baseUrl : "scripts",
+                    mainConfigFile: "scripts/main.js",
+                    name : "SlotMachine",
+                    out : "target/app-<%= pkg.version %>.js",
+                    optimize : "none"
+                }
+            }
+        },
+        uglify: {
+            my_target: {
+                files: {
+                    'target/app-<%= pkg.version %>.min.js': ['target/app-<%= pkg.version %>.js']
+                }
+            }
+        },
 		jasmine : {
             src : 'scripts/*.js',
             options : {
                 specs : ['tests/*.spec.js'],
-                //helpers : 'jtest/helpers/*.js',
                 template: require('grunt-template-jasmine-requirejs'),
                 templateOptions: {
                     requireConfigFile: 'scripts/main.js',
@@ -29,15 +46,21 @@ module.exports = function(grunt) {
 	        options: {
 	          nospawn: true
 	        }
-	      }
+	      },
+          requirejs : {
+            files : ['js/*.js','js/**/*.js'],
+                tasks : ['requirejs']
+          }
 	    }
 	});
 
 	grunt.loadNpmTasks('grunt-contrib-less');
 	grunt.loadNpmTasks('grunt-contrib-watch');
 	grunt.loadNpmTasks('grunt-contrib-jasmine');
+    grunt.loadNpmTasks('grunt-contrib-requirejs');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
 
     grunt.registerTask('test', ['jasmine']);
-	grunt.registerTask('build', ['less', 'jasmine']);
+	grunt.registerTask('build', ['less', 'requirejs', 'uglify', 'jasmine']);
 }
 
